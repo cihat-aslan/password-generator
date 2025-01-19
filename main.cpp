2b77241992d694781565cc0fd38b009678898c0c
cihat-aslan <cihataslanbiz@gmail.com>
2025-01-20 01:28:12 +0300

    editing

--- main.cpp
+++ main.cpp
@@ -2,36 +2,45 @@
 #include <limits>
 #include <cstdlib>
 #include <ctime>
+#include <conio.h>
 using namespace std;
 
 int main() {
 
-    string karakterler = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.,-_/+*!@#$%^&";
-    int boyut = 0;
+    string characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.,-_/+*!@#$%^&";
+    int leng = 0;
     while (true) {
 
-        cout << "sifrenizin boyutunu girin (6-32):";
-        cin >> boyut;
+        cout << "enter your password length (6-32):";
+        cin >> leng;
 
         if (cin.fail()) {
             cin.clear();
             cin.ignore(numeric_limits<streamsize>::max(), '\n');
-            cout << "gecersiz karakter veya boyutu buyuk girdiniz. lutfen 6-32 arasında bir sayı girin." << endl;
+            cout << "invalid character or too long integer. please choose a number between 6-32." << endl;
 
         }
-        else if (boyut < 6 || boyut > 32) {
-            cout << "gecersiz bir sayı girdiniz. lutfen 6-32 arasında bir sayı girin." << endl;
+        else if (leng < 6 || leng > 32) {
+            cout << "invalid number. please choose a number between 6-32." << endl;
         }else {
             break;
         }
     }
 
     srand(time(nullptr));
-    cout << "sifreniz olusturuldu:";
-    for (int i = 0; i < boyut; i++) {
-        cout << karakterler[rand() % karakterler.size()];
+    cout << "your password generated: ";
+    for (int i = 0; i < leng; i++) {
+        cout << characters[rand() % characters.size()];
     }
+    cout << endl;
 
+#ifdef _WIN32
+    cout << "Press any key to close the console..." << endl;
+    _getch();
+#else
+    cout << "Press Enter to close the console..." << endl;
+    cin.get();
+#endif
 
     return 0;
 }
